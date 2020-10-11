f2b34245ac3abb03a5acf6b7b9755679ab980e19
jud3v <minipacha972@outlook.fr>
2020-10-11 17:52:43 +0200

    Migration, JWT Auth, Model, Controller
    Migration: adding migration for models User, Receipt, Network,
    Model: scaffolding model,
    JWT: adding package jwt-auth
    Controller: adding api controller, authcontroller,

---
+++ app/Models/Network.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Network extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'url',
+        'name',
+        'user_id'
+    ];
+
+    protected $guarded = [
+        'id',
+        'user_id'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
